ee2ac2afcf0897b012cd405e082fd0e7ea3b411d
Patrick Liesveld <pliesveld@users.noreply.github.com>
2019-07-07 19:43:36 -0400

    wip; adding GDNative module

--- godot/core/main_container.gd
+++ godot/core/main_container.gd
@@ -6,6 +6,10 @@ func _ready():
 		"s": 4
 	})
 	
+	var rng = RandomNumberGenerator.new()
+	rng.set_seed(42)
+	print(rng.randi())
+	
 	event_bus.subscribe("crash_event", self, "_handle_crash_event")
 	#$Timer.time_left = 10.0
 	$Timer.wait_time = 10
